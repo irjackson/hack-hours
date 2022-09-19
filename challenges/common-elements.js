cdc447769f9ae307fb120672308781133ca7e733
Ian Jackson <irjackson9@gmail.com>
2022-09-19 13:22:25 -0700

    9/19/22 HH

--- challenges/common-elements.js
+++ challenges/common-elements.js
@@ -16,29 +16,26 @@ commonElements(arr1, arr2, arr3) -> [2, 3, 2000, 'dog']
 */
 
 const commonElements = (...args) => {
-  const output = [];
-  console.log(args);
-  for(let i = 0; i < args.length; i++){
-    const curr = args[i];
-    const next = args[i + 1];
-    console.log(curr);
-    console.log(next);
-    if(args[i] === undefined && output.length === 0){
-      return 'Nothing in Common!'
+  const output = {};
+  const result = [];
+  const compare = args.length;
+  for (let i = 0; i < args.length; i++){
+    for (let j = 0; j < args[i].length; j++){
+      output[args[i][j]] = output[args[i][j]] + 1|| 1;
+      console.log(output)
     }
-    for(let el of curr){
-      console.log(el);
-      if(curr.includes(next)){
-        output.push(next)
-      }
-    }
-    console.log(output)
   }
+  for (const prop in output){
+    if (output[prop] === compare){
+      result.push(prop)
+    }
+  }
+  return result;
 }
 arr1 = [2, 10,'cat', 3, 99, 2000, 'dog', 'lion'];
 arr2 = [3, 7, 2, 2000, 1, 'dog', 'cat'];
 arr3 = [2, 100, 2000, 'dog', 3, 'lion'];
-commonElements(arr1, arr2, arr3);
+console.log(commonElements(arr1, arr2, arr3));
 
 /*
 ** Extension **
