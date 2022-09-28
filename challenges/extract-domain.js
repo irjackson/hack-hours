812c271817b0edc04238db7d60b05d3c9e009165
Ian Jackson <irjackson9@gmail.com>
2022-09-28 16:40:17 -0700

    9/28/22 HH

---
+++ challenges/extract-domain.js
@@ -0,0 +1,40 @@
+// Write a function that when given a URL as a string, parses out just the domain name and returns it as a string. For example:
+// * url = "http://github.com/carbonfive/raygun" -> domain name = "github"
+// * url = "http://www.zombie-bites.com"         -> domain name = "zombie-bites"
+// * url = "https://www.cnet.com"                -> domain name = cnet"
+
+function extractDomain(domain){
+  const split = domain.split('');
+  let check = [];
+  const extract = [];
+  let capture = false;
+  let dotCom = 0;
+  
+  for (let i = split.length -1; i >= 0; i--){
+    if (check.join('').includes("moc.")){
+      dotCom = i;
+      break;
+    }
+    if (check.join('').includes("vt.")){
+      dotCom = i;
+      break;
+    }
+    if (check.join('').includes("gro.")){
+      dotCom = i;
+      break;
+    }
+    check.push(split[i]);
+  }
+  while (split[dotCom] !== '.' && split[dotCom] !== '/'){
+    if (split[dotCom] === undefined) break;
+    extract.push(split[dotCom]);
+    dotCom = dotCom - 1;
+  }
+  return extract.reverse().join('');
+}
+
+console.log(extractDomain("https://www.cnet.com"));
+console.log(extractDomain("http://github.com/carbonfive/raygun"));
+console.log(extractDomain("http://www.zombie-bites.com"));
+console.log(extractDomain("https://developer.mozilla.org"));
+console.log(extractDomain("https://www.google.com/"));
