f22c8aa0de489076b6d6ad1e52ac88083eaaf7df
Ian Jackson <irjackson9@gmail.com>
2022-10-21 16:12:07 -0700

    10/21/22 HH

---
+++ challenges/number-letter.js
@@ -0,0 +1,89 @@
+// Your task is to write a function named do_math that receives a single argument. 
+// This argument is a string that contains multiple whitespace delimited numbers. 
+// Each number has a single alphabet letter somewhere within it.
+
+// Example : "24z6 1x23 y369 89a 900b"
+
+// As shown above, this alphabet letter can appear anywhere within the number. You have to extract the letters and sort the numbers 
+// according to their corresponding letters.
+
+// Example : "24z6 1x23 y369 89a 900b" will become 89 900 123 369 246 (ordered according to the alphabet letter)
+
+// Here comes the difficult part, now you have to do a series of computations on the numbers you have extracted.
+
+//     The sequence of computations are + - * /. Basic math rules do NOT apply, you have to do each computation in exactly this order.
+//     This has to work for any size of numbers sent in (after division, go back to addition, etc).
+//     In the case of duplicate alphabet letters, you have to arrange them according to the number that appeared first in the input string.
+//     Remember to also round the final answer to the nearest integer.
+
+// Examples :
+// "24z6 1x23 y369 89a 900b" = 89 + 900 - 123 * 369 / 246 = 1299
+// "24z6 1z23 y369 89z 900b" = 900 + 369 - 246 * 123 / 89 = 1414
+// "10a 90x 14b 78u 45a 7b 34y" = 10 + 45 - 14 * 7 / 78 + 90 - 34 = 60
+
+function do_math(str){
+  const nums = str.split(' ');
+  let letters = []
+  console.log(nums.sort());
+  //Sort by the numbers by letter
+  for (let i = 0; i < nums.length; i++){
+    letters.push(nums[i].match(/[a-z]/gi));
+  };
+  letters = letters.flat().sort();
+  console.log(letters);
+  let sortedNums = [];
+  for (let i = 0; i < letters.length; i++){
+    for(let j = 0; j <= nums[i].length; j++){
+      if (nums[j].includes(letters[i])){
+        sortedNums.push(nums[j]);
+      };
+    };
+  };
+  console.log(sortedNums);
+  //Remove letters from sorted numbers
+  for (let i = 0; i < sortedNums.length; i++){
+    let curr = sortedNums[i].split('');
+    let newNum = '';
+    for (let j = 0; j < curr.length; j++){
+      if (!curr[j].match(/[a-z]/gi)){
+        newNum += curr[j];
+      }
+    }
+    sortedNums[i] = newNum;
+  }
+  console.log(sortedNums)
+
+  //Iterate through sortedNums and apply operators to 0th index
+  let calculate = Number(sortedNums.shift());
+  let operators = ['+', '-', '*', '/'];
+  let counter = 0;
+  console.log(calculate);
+  while (sortedNums.length){
+    switch(operators[counter]){
+      case '+':
+        calculate += Number(sortedNums[0]);
+        break;
+      case '-':
+        calculate -= Number(sortedNums[0]);
+        break;
+      case '*':
+        calculate *= Number(sortedNums[0]);
+        break;
+      case '/':
+        calculate = (calculate / Number(sortedNums[0]));
+        break;
+    }
+    counter++;
+    //check if counter is out of bounds
+    if(counter > 3){
+      counter = 0;
+    }
+    sortedNums.shift();
+    console.log(sortedNums);
+    console.log(calculate);
+  }
+  return parseInt(calculate);
+};
+
+console.log(do_math("24z6 1x23 y369 89a 900b"));
+console.log(do_math("10a 90x 14b 78u 45a 7b 34y"));
