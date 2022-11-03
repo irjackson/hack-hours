9006c7b6d1dcf502d3e73c8af16b6d60ca53a037
Ian Jackson <irjackson9@gmail.com>
2022-11-03 15:58:27 -0700

    11/3/22 HH

---
+++ challenges/persistence.js
@@ -0,0 +1,30 @@
+// Write a function, persistence, that takes in a positive parameter num and returns its multiplicative 
+// persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
+
+// For example (Input --> Output):
+
+// 39 --> 3 (because 3*9 = 27, 2*7 = 14, 1*4 = 4 and 4 has only one digit)
+// 999 --> 4 (because 9*9*9 = 729, 7*2*9 = 126, 1*2*6 = 12, and finally 1*2 = 2)
+// 4 --> 0 (because 4 is already a one-digit number)
+
+function persistence(num, times = 0){
+  if (num < 0) return 'Error: must be positive number';
+  if (num < 10) return times;
+  let stringify = num.toString();
+  let digitArr = [];
+  for (let i = 0; i < stringify.length; i++){
+    digitArr.push(stringify[i]);
+  }
+
+  let result = Number(digitArr.shift());
+  while (digitArr.length){
+    result *= Number(digitArr[0]);
+    digitArr.shift();
+  }
+  times += 1;
+  return persistence(result, times);
+};
+
+console.log(persistence(39));
+console.log(persistence(999));
+console.log(persistence(4));
