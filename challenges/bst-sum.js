299478ff8668e6109dbb4155eb4a36e7cd460bf8
Ian Jackson <irjackson9@gmail.com>
2022-11-07 16:36:48 -0800

    11/7/22 HH

--- challenges/bst-sum.js
+++ challenges/bst-sum.js
@@ -13,9 +13,19 @@ function.
 
 */
 
-const bstSum = tree => {
+const bstSum = root => {
   const leftVal = root.left ? bstSum(root.left) : 0;
   const rightVal = root.right ? bstSum(root.right) : 0;
 
-  return root.value + root.left + root.right;
+  return root.value + leftVal + rightVal;
 };
+
+let root = new BinarySearchTree(4);
+root.left = new BinarySearchTree(2);
+root.right = new BinarySearchTree(7);
+root.left.left = new BinarySearchTree(1);
+root.left.right = new BinarySearchTree(3);
+root.right.right = new BinarySearchTree(9);
+root.right.right.left = new BinarySearchTree(8);
+
+console.log(bstSum(root));
