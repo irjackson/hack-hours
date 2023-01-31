38466ce88679d543874fec5334acd6c5836b6340
Ian Jackson <irjackson9@gmail.com>
2023-01-30 17:49:38 -0800

    1/30/23 HH

---
+++ challenges/conveniencestore.js
@@ -0,0 +1,36 @@
+//Given a total due and an array representing the amount of change in your pocket, determine whether or not you are 
+// able to pay for the item. Change will always be represented in the following order: quarters, dimes, nickels, pennies.
+
+// To illustrate: changeEnough([25, 20, 5, 0], 4.25) should yield true, since having 25 quarters, 20 dimes, 5 nickels and 0 pennies 
+// gives you 6.25 + 2 + .25 + 0 = 8.50.
+
+//Examples:
+// changeEnough([2, 100, 0, 0], 14.11) ➞ false
+// changeEnough([0, 0, 20, 5], 0.75) ➞ true
+// changeEnough([30, 40, 20, 5], 12.55) ➞ true
+
+// quarter: 25 cents / $0.25
+// dime: 10 cents / $0.10
+// nickel: 5 cents / $0.05
+// penny: 1 cent / $0.01
+
+function changeEnough(pocket, totalDue){
+  let coinTotal = 0;
+  for (let i = 0; i < pocket.length; i++){
+    if (i === 0){
+      coinTotal += (.25 * pocket[i]);
+    };
+    if (i === 1){
+      coinTotal += (.10 * pocket[i]);
+    };
+    if (i === 2){
+      coinTotal += (.05 * pocket[i]);
+    };
+    if (i === 3){
+      coinTotal += (.01 * pocket[i]);
+    };
+  }
+  return coinTotal > totalDue;
+};
+
+console.log(changeEnough([2, 100, 0, 0], 14.11));
