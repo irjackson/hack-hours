352a62fd5156cf7fcf11f21410c4939e07acdf43
Ian Jackson <irjackson9@gmail.com>
2023-01-31 17:13:37 -0800

    1/31/23 HH

---
+++ challenges/captofront.js
@@ -0,0 +1,21 @@
+// Create a function that moves all capital letters to the front of a word.
+
+// Examples
+// capToFront("hApPy") ➞ "APhpy"
+// capToFront("moveMENT") ➞ "MENTmove"
+// capToFront("shOrtCAKE") ➞ "OCAKEshrt"
+
+function capToFront(str){
+  if (typeof str !== 'string') return 'ERROR: input must be a string';
+  let capArray = [];
+  let lowerArray = [];
+  for (let i = 0; i < str.length; i++){
+    if (str[i] === str[i].toUpperCase()){
+      capArray.push(str[i]);
+    }
+    else {
+      lowerArray.push(str[i]);
+    }
+  }
+  return capArray.join('') + lowerArray.join('');
+};
