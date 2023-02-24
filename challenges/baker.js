0eb54410d09590476143f6771b798182768cb123
Ian Jackson <irjackson9@gmail.com>
2023-02-23 16:54:56 -0800

    2/23/23 HH

--- challenges/baker.js
+++ challenges/baker.js
@@ -29,4 +29,4 @@ function cakes(recipe, ingredients){
 
 console.log(cakes({flour: 500, sugar: 200, eggs: 1}, {flour: 1200, sugar: 1200, eggs: 5, milk: 200}));
 console.log(cakes({apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100}, {sugar: 500, flour: 2000, milk: 2000})); 
-console.log(cakes([2, 4], [23, 'cake']))
+console.log(cakes([2, 4], [23, 'cake']));
