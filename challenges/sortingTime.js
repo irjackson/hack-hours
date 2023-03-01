df5c98f74a8e9f5f1f809beb08c1aac0cd97dcee
Ian Jackson <irjackson9@gmail.com>
2023-02-28 16:59:49 -0800

    2/28/2023 HH

--- challenges/sortingTime.js
+++ challenges/sortingTime.js
@@ -3,12 +3,10 @@
 
 // Create a function that takes an array of integers as an argument and returns the same array in ascending order. 
 // Using sort() would be easy, but for this challenge YOU have to sort the array creating your own algorithm.
+
 // Examples
-
 // sortArray([2, -5, 1, 4, 7, 8]) ➞ [-5, 1, 2, 4, 7, 8]
-
 // sortArray([23, 15, 34, 17, -28]) ➞ [-28, 15, 17, 23, 34]
-
 // sortArray([38, 57, 45, 18, 47, 39]) ➞ [18, 38, 39, 45, 47, 57]
 
 // Notes
@@ -19,19 +17,26 @@
 //     This is a challenge to enhance your ability, using the sort built-in won't enhance your skills.
 
 function sortArray(array, result = []){
+  if (array.length === 0) return result;
+  let sorter = [];
   let lowest = array[0];
-  let counter = array.length;
-  while(counter > 0){
-    for (let i = 0; i < array.length; i++){
-      console.log(lowest);
-      if (array[i] < array[i+1] && array[i] < lowest){
-        lowest = array[i];
-      };
+  for (let i = 0; i < array.length; i++){
+    console.log(lowest);
+    if (array[i] < lowest && !result.includes(array[i])){
+      lowest = array[i];
     };
-    counter -= 1;
-    result.push(lowest)
+  };
+  for (let j = 0; j < array.length; j++){
+    if (array[j] === lowest){
+      continue;
+    } else {
+      sorter.push(array[j])
+    }
   }
-  return result;
+  result.push(lowest);
+  return sortArray(array = sorter, result);
 };
 
+console.log(sortArray([2, -5, 1, 4, 7, 8]));
+console.log(sortArray([23, 15, 34, 17, -28]));
 console.log(sortArray([2, -5, 1, 4, 7, 8]));
