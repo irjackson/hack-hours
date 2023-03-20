b40898a11d751b5de72cee0c949609d6b90f02d3
Ian Jackson <irjackson9@gmail.com>
2023-03-20 14:43:24 -0700

    3/20/23 HH

---
+++ challenges/ungroupData.js
@@ -0,0 +1,66 @@
+// You volunteered to help out teaching a preschool in your area! You were given an array of all students and 
+// some important data about them, grouped by their teacher. Create a function that will ungroup every student so you can 
+// look at their details individually.
+
+// Example
+// ungroupStudents([{
+//   teacher: "Ms. Car",
+//   data: [{
+//      name: "James",
+//      emergenceNumber: "617-771-1082",
+//   }, {
+//      name: "Alice",
+//      alergies: ["nuts", "carrots"],
+//   }],
+// }, {
+//   teacher: "Mr. Lamb",
+//   data: [{
+//     name: "Aaron",
+//     age: 3
+//   }]
+// }]) ➞ [{
+//   teacher: "Ms. Car",
+//   name: "James",
+//   emergencyNumber: "617-771-1082",
+// }, {
+//   teacher: "Ms. Car",
+//   name: "Alice",
+//   alergies: ["nuts", "carrots"],
+// }, {
+//   teacher: "Mr. Lamb",
+//   name: "Aaron",
+//   age: 3,
+// }]
+
+function ungroupStudents(studentData){
+  let result = [];
+  for (let i = 0; i < studentData.length; i++){
+    for (let student of studentData[i].data){
+      const newEntry = {};
+      // console.log(student);
+      newEntry.teacher = studentData[i].teacher;
+      // console.log(newEntry.teacher);
+      const addDetails = Object.assign(newEntry, student);
+      // console.log(newEntry);
+      result.push(newEntry);
+    };
+  };
+  return result;
+};
+
+console.log(ungroupStudents([{
+    teacher: "Ms. Car",
+    data: [{
+       name: "James",
+       emergenceNumber: "617-771-1082",
+    }, {
+       name: "Alice",
+       alergies: ["nuts", "carrots"],
+    }],
+  }, {
+    teacher: "Mr. Lamb",
+    data: [{
+      name: "Aaron",
+      age: 3
+    }]
+  }]));
