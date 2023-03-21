f159f4955da6579dd1386726f693e0a3918c0bb5
Ian Jackson <irjackson9@gmail.com>
2023-03-21 16:13:08 -0700

    3/21/23 HH

--- challenges/getNotes.js
+++ challenges/getNotes.js
@@ -19,11 +19,10 @@
 
 function getNotesDistribution(students) {
   const noteDist = {};
-  const validNotes = [1, 2, 3, 4, 5];
   for (let i = 0; i < students.length; i++){
     for (let num of students[i].notes){
       console.log(num);
-      if (validNotes.includes(num)){
+      if (num < 6 && num > 0){
         noteDist[`${num}`] = noteDist[`${num}`] + 1 || 1;
       }
     }
@@ -40,4 +39,19 @@ console.log(getNotesDistribution([
       "name": "John",
       "notes": [3, 2, 5, 0, -3]
     }
+  ]));
+
+  console.log(getNotesDistribution([
+    {
+      "name": "Dave",
+      "notes": [3, 1, -1, -8, -6]
+    },
+    {
+      "name": "Mary",
+      "notes": [1, 5, 4, -10, 2]
+    },
+    {
+      "name": "Sam",
+      "notes": [5, 6, 2, 4, -9]
+    },
   ]));
