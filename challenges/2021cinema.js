3797fb3eaec4d6828bb9e1ddb53a12d1cc316dca
Ian Jackson <irjackson9@gmail.com>
2023-03-31 16:57:31 -0700

    3/31/23 HH

---
+++ challenges/2021cinema.js
@@ -0,0 +1,40 @@
+// Given an array of seats, return the maximum number of new people which can be seated, as long as there is at least a gap 
+// of 2 seats between people.
+
+//     Empty seats are given as 0.
+//     Occupied seats are given as 1.
+//     Don't move any seats which are already occupied, even if they are less than 2 seats apart. Consider only the gaps 
+//     between new seats and existing seats.
+
+// Examples
+  // maximumSeating([0, 0, 0, 1, 0, 0, 1, 0, 0, 0]) ➞ 2
+    // [1, 0, 0, 1, 0, 0, 1, 0, 0, 1]
+  // maximumSeating([0, 0, 0, 0]) ➞ 2
+    // [1, 0, 0, 1]
+  // maximumSeating([1, 0, 0, 0, 0, 1]) ➞ 0
+    // There is no way to have a gap of at least 2 chairs when adding someone else.
+
+// Notes
+//     Notice how there may be several possibilities for assigning seats to people, but these cases won't affect the results.
+//     All seats will be valid.
+
+function maximumSeating(arr){
+  let openSeats = 0;
+  for (i = 0; i < arr.length; i++){
+    if (arr[i] === 1){
+      let j = i - 2;
+      while (arr[j] === 0){
+        j++;
+      };
+      console.log(j)
+      if (j > 2){
+        openSeats++;
+      };
+    }
+  };
+  return openSeats;
+};
+
+console.log(maximumSeating([0, 0, 0, 1, 0, 0, 1, 0, 0, 0]));
+// console.log(maximumSeating([0, 0, 0, 0]));
+// console.log(maximumSeating([1, 0, 0, 0, 0, 1]));
