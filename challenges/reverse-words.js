7d96af6535e4d8bf2bfb83f3b232f3849c746c19
Ian Jackson <irjackson9@gmail.com>
2023-04-17 15:45:59 -0700

    4/17/23 HH

---
+++ challenges/reverse-words.js
@@ -0,0 +1,40 @@
+// Given an input string, reverse the string word by word, the first word will be the last, and so on.
+// Examples
+
+// reverseWords(" the sky is blue") ➞ "blue is sky the"
+
+// reverseWords("hello   world!  ") ➞ "world! hello"
+
+// reverseWords("a good example") ➞ "example good a"
+
+// Notes
+//   A word is defined as a sequence of non-space characters.
+//   The input string may contain leading or trailing spaces. However, your reversed string should not contain leading or trailing spaces.
+//   You need to reduce multiple spaces between two words to a single space in the reversed string.
+//   Try to solve this in linear time. 
+
+function reverseWords(string){
+  let curr = [];
+  let reversed = [];
+  for (let i = 0; i <= string.length; i++){
+    console.log(reversed);
+    if (curr[0] === ' '){
+      curr = [];
+    };
+    if(string[i] === ' ' && curr.length > 0 || string[i] === undefined){
+      if(curr == ''){
+        return reversed.join(' ');
+      };
+      reversed.unshift(curr.join(''));
+      curr = [];
+    };
+    if (string[i] === undefined){
+      return reversed.join(' ');
+    }; 
+    curr.push(string[i]);
+  };
+};
+
+console.log(reverseWords(" the sky is blue"));
+console.log(reverseWords("hello   world!  "));
+console.log(reverseWords("a good example"));
