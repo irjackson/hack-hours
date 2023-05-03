fd30c4df8481d17c1fedcd9c21ba3ead98f25272
Ian Jackson <irjackson9@gmail.com>
2023-05-02 17:51:05 -0700

    5/2/23 HH

---
+++ challenges/wordtosent.js
@@ -0,0 +1,35 @@
+// Create a function that turns an array of words into a comma separated list, where the last word is separated by the word "and".
+
+// Examples
+//   wordsToSentence(["edabit"]) ➞ "edabit"
+//   wordsToSentence(["Hello", "", "Bye"]) ➞ "Hello and Bye"
+//   wordsToSentence(["Hello", "Bye", "See you soon"]) ➞ "Hello, Bye and See you soon"
+
+// Notes
+//   null values, empty arrays or arrays with only empty or null values should return an empty string (e.g. "").
+
+function wordsToSentence(words){
+  if (!words || words.length === 0 || words[0] === "") return "";
+  if (words[0] && words.length === 1) return words[0];
+
+  words = words.filter(word => word !== "");
+
+  if (words.length === 2){
+    return words.join(" and ");
+  };
+
+  let count = 1;
+  let length = words.length;
+  while (count < length - 1){
+    words.splice(count, 0, ", ");
+    length += 1;
+    count += 2;
+  }
+  
+  words.splice(words.length - 1, 0, " and ")
+  return words.join('');
+};
+
+console.log(wordsToSentence(["edabit"]))
+console.log(wordsToSentence(["Hello", "", "Bye"]));
+console.log(wordsToSentence(["Hello", "Bye", "See you soon"]))
