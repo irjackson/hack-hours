00a105dc9bd73eaf7fcfabe270753b0dea08e033
Ian Jackson <irjackson9@gmail.com>
2023-05-22 17:49:28 -0700

    5/22/23 HH

---
+++ challenges/fieldgoal.js
@@ -0,0 +1,53 @@
+// In (American) Football, a team can score if they manage to kick a ball through the goal 
+// (i.e. above the crossbar and between the uprights).
+
+// Create a function that returns true if the ball 0 goes through the goal. You will be given an array of arrays.
+  // Examples
+  //   isGoalScored([
+  //     ["  #     #  "],
+  //     ["  #  0  #  "],
+  //     ["  #     #  "],
+  //     ["  #######  "],
+  //     ["     #     "],
+  //     ["     #     "],
+  //     ["     #     "]
+  //   ]) ➞ true
+
+  //   isGoalScored([
+  //     ["  #0    #  "],
+  //     ["  #     #  "],
+  //     ["  #     #  "],
+  //     ["  #######  "],
+  //     ["     #     "],
+  //     ["     #     "],
+  //     ["     #     "]
+  //   ]) ➞ true
+
+  //   isGoalScored([
+  //     ["  #     #  "],
+  //     ["  #     #  "],
+  //     ["  #     # 0"],
+  //     ["  #######  "],
+  //     ["     #     "],
+  //     ["     #     "],
+  //     ["     #     "]
+  //   ]) ➞ false
+
+// Notes
+//     All goals will be of the same size.
+//     All arrays will be of equal length (11).
+//     A team can never score if it hits the crossbar or goes underneath it.
+
+function isGoalScored(goal){
+  
+};
+
+console.log(isGoalScored([
+      ["  #     #  "],
+      ["  #  0  #  "],
+      ["  #     #  "],
+      ["  #######  "],
+      ["     #     "],
+      ["     #     "],
+      ["     #     "]
+    ]));
