6062b0d74ee357bfcf9f32f604e88ffe0d93b031
Ian Jackson <irjackson9@gmail.com>
2023-06-02 13:48:56 -0700

    6/2/23 HH

---
+++ challenges/ruglayers.js
@@ -0,0 +1,69 @@
+// Write a function that counts how many concentric layers a rug has.
+
+// Examples
+
+// countLayers([
+//   "AAAA",
+//   "ABBA",
+//   "AAAA"
+// ]) ➞ 2
+
+// countLayers([
+//   "AAAAAAAAA",
+//   "ABBBBBBBA",
+//   "ABBAAABBA",
+//   "ABBBBBBBA",
+//   "AAAAAAAAA"
+// ]) ➞ 3
+
+// countLayers([
+//   "AAAAAAAAAAA",
+//   "AABBBBBBBAA",
+//   "AABCCCCCBAA",
+//   "AABCAAACBAA",
+//   "AABCADACBAA",
+//   "AABCAAACBAA",
+//   "AABCCCCCBAA",
+//   "AABBBBBBBAA",
+//   "AAAAAAAAAAA"
+// ]) ➞ 5
+
+// Notes
+
+//     Multiple layers can share the same component so count them separately (example #2).
+//     Layers will be horizontally and vertically symmetric.
+//     There will be at least one layer for each rug.
+
+function countLayers(rug){
+  const result = new Set();
+  for (let layer of rug){
+    result.add(layer);
+  }
+  return result.size;
+};
+
+console.log(countLayers([
+    "AAAA",
+    "ABBA",
+    "AAAA"
+  ]));
+
+console.log(countLayers([
+    "AAAAAAAAA",
+    "ABBBBBBBA",
+    "ABBAAABBA",
+    "ABBBBBBBA",
+    "AAAAAAAAA"
+  ]));
+
+console.log(countLayers([
+    "AAAAAAAAAAA",
+    "AABBBBBBBAA",
+    "AABCCCCCBAA",
+    "AABCAAACBAA",
+    "AABCADACBAA",
+    "AABCAAACBAA",
+    "AABCCCCCBAA",
+    "AABBBBBBBAA",
+    "AAAAAAAAAAA"
+  ]));
