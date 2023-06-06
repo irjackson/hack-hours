a4e8dbb7281fdb12f8f9604484c5a93de51cc8ff
Ian Jackson <irjackson9@gmail.com>
2023-06-06 13:39:17 -0700

    6/6/23 HH

--- challenges/doubleswap.js
+++ challenges/doubleswap.js
@@ -13,8 +13,18 @@
 
 // Both characters will show up at least once in the string.
 
-function doubleSwap(){
-
+function doubleSwap(str, c1, c2){
+  let newStr = '';
+  for (let i = 0; i < str.length; i++){
+    if (str[i] === c1) newStr += c2;
+    else if (str[i] === c2) newStr += c1;
+    else {
+      newStr += str[i];
+    }
+  };
+  return newStr;
 };
 
-console.log(doubleSwap());
+console.log(doubleSwap("aabbccc", "a", "b"));
+console.log(doubleSwap("random w#rds writt&n h&r&", "#", "&"));
+console.log(doubleSwap("128 895 556 788 999", "8", "9"));
