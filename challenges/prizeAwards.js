55c2fecc75410af15b0c9f9199f2580a0fbda669
Ian Jackson <irjackson9@gmail.com>
2023-06-09 14:52:56 -0700

    6/9/23 HH

--- challenges/prizeAwards.js
+++ challenges/prizeAwards.js
@@ -45,8 +45,39 @@
 //     There will always be at least three participants to recieve awards.
 //     No number of points will be the same amongst participants.
 
-function awardPrizes(){
-
+function awardPrizes(names){
+  const medals = ['Gold', 'Silver', 'Bronze'];
+  const ranking = Object.entries(names).sort((a, b) => b[1] - a[1]);
+  const result = {};
+  while (ranking.length){
+    if (medals.length){
+      result[ranking[0][0]] = medals[0];
+      medals.shift();
+    } else {
+      result[ranking[0][0]] = 'Participation';
+    }
+    ranking.shift();
+  };
+  return result;
 };
 
-console.log(awardPrizes());
+console.log(awardPrizes({
+  "Joshua" : 45,
+  "Alex" : 39,
+  "Eric" : 43
+}));
+
+console.log(awardPrizes({
+  "Person A" : 1,
+  "Person B" : 2,
+  "Person C" : 3,
+  "Person D" : 4,
+  "Person E" : 102
+}));
+
+console.log(awardPrizes({
+  "Mario" : 99,
+  "Luigi" : 100,
+  "Yoshi" : 299,
+  "Toad" : 2
+}));
