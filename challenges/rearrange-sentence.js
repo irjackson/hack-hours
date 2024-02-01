28b7570af5d5451224bf051fe12db339662cedfe
Ian Jackson <irjackson9@gmail.com>
2024-02-01 15:15:45 -0800

    2/1/24 HH

---
+++ challenges/rearrange-sentence.js
@@ -0,0 +1,33 @@
+// Given a sentence with numbers representing a word's location embedded within each word, return the sorted sentence.
+
+// Examples
+//   rearrange("is2 Thi1s T4est 3a") ➞ "This is a Test"
+
+//   rearrange("4of Fo1r pe6ople g3ood th5e the2") ➞ "For the good of the people"
+
+//   rearrange(" ") ➞ ""
+
+// Notes
+//   Only the integers 1-9 will be used.
+
+function rearrange(sentence){
+  if (!sentence || sentence === " ") return "";
+  let wordList = sentence.split(" ");
+  let sortedSentence = Array(wordList.length);
+  console.log(wordList);
+  console.log(sortedSentence);
+  for (word of wordList){
+    for (let i = 0; i < word.length; i++){
+      console.log(Number(word[i]))
+      if (Number(word[i])){
+        let removedNum = word.replace(/\d/g, '')
+        console.log(removedNum);
+        sortedSentence[Number(word[i])-1] = removedNum;
+      };
+    };
+  };
+  return sortedSentence.join(" ");
+};
+
+console.log(rearrange("is2 Thi1s T4est 3a"));
+console.log(rearrange(" "));
