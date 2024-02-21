15c5e6e6fd0620caa0ee0c253902ae8e3c4767ae
Ian Jackson <irjackson9@gmail.com>
2024-02-21 13:30:10 -0800

    2/20/24 HH

--- challenges/h-v-m.js
+++ challenges/h-v-m.js
@@ -1,4 +1,5 @@
-// You are implementing a battle system for a simple role-playing game. A hero is fighting various monsters, and you have to determine the battle's outcome for each engaged skirmish. Hero and monsters share some stats:
+// You are implementing a battle system for a simple role-playing game. A hero is fighting various monsters, and 
+// you have to determine the battle's outcome for each engaged skirmish. Hero and monsters share some stats:
 
 //     HP or Health Points: The amount of damage the character can sustain. If it reaches 0 (or less) the character dies.
 //     ATT or Attack: The character offensive capacity.
@@ -7,7 +8,7 @@
 // For either hero and monster, the damage inflicted is calculated subtracting the opponent's defense score from 
 // the attacker doubled attack score.
 
-// In each round hero attacks first, and his damage rate is subtracted from the monster's HP.
+//  In each round hero attacks first, and his damage rate is subtracted from the monster's HP.
 //  If the monster survives (HP > 0), it is his turn to attack. If the hero survives (HP > 0), a new round starts. 
 //  The hero can also have some POT, or Healing Potions, in his backpack: only when his HP is equal to or lower than 10, a potion 
 //  can (and must) be used for regaining 10 HP at the start of a new round. When the hero heals, he can't attack, but he 
@@ -16,8 +17,8 @@
 // Given an object containing the character's stats (with p-prefix ones being the hero's stats and m-prefix ones being the monster's) you 
 // must return a string:
 
-//     "Victory in x rounds" if the hero wins.
-//     "Game Over in x rounds" if the monster wins.
+//  "Victory in x rounds" if the hero wins.
+//  "Game Over in x rounds" if the monster wins.
 
 // (with x being the number of rounds elapsed)
 // Examples
