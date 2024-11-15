f08352a7153b086b780659a8b8910166ec64045b
Ian Jackson <irjackson9@gmail.com>
2024-11-14 16:13:17 -0800

    11/14/24 HH

---
+++ challenges/either404.js
@@ -0,0 +1,24 @@
+// Given is an array 'numbers' with integers. 
+// Return true if a zero is next to a zero or a four is next to a four. Return false if both occur.
+
+function either404(numbers){
+  let match = null;
+  let result = null;
+  let doubleMatch = {4: null, 0: null};
+  for (let i = 0; i < numbers.length; i++){
+     if (numbers[i] === 4 || numbers[i] === 0){
+      match = numbers[i];
+      if (numbers[i-1] === match || numbers[i+1] === match){
+        result = true;
+        delete doubleMatch[match];
+      };
+     };
+  };
+  console.log(Object.keys(doubleMatch));
+  if (Object.keys(doubleMatch).length === 0) return false;
+  return result;
+};
+
+console.log(either404([2,8,4,4]));
+console.log(either404([0,0,3,6,4,4]));
+console.log(either404([1,4,4,0,2,0,9]));
